cb1cdc3f608c091587ee781005e8263c5961e998
github username <github email address>
2020-03-17 22:40:09 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,17 @@
-require 'pry'
+# require 'pry'
 
-# require_relative '../lib/concerns/findable'
-# require_relative '../lib/concerns/memorable'
-# require_relative '../lib/concerns/paramable'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/paramable'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
+
+
+me = Artist.new
+
+me.name = "Josh"
+
+Artist.find_by_name("Josh")
+
+# binding.pry
